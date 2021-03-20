7c51061561b4a141fc75e93ddc9a155aeea34f89
Elard Huaraca <elard.sd.9@gmail.com>
2021-03-20 10:53:33 -0700

    tarea terminada

--- TareaLogin/TareaLogin/ViewController.swift
+++ TareaLogin/TareaLogin/ViewController.swift
@@ -9,11 +9,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var cardlabel: UILabel!
+    @IBOutlet weak var btnLogin: UIButton!
+    
     override func viewDidLoad() {
+        
+        cardlabel.layer.masksToBounds = true
+        cardlabel.layer.cornerRadius = 10
+        btnLogin.layer.masksToBounds = true
+        btnLogin.layer.cornerRadius = 15
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
 }
-
