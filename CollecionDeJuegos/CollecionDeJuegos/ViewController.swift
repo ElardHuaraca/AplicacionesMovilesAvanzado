1d86e8d939a05f973989a35a4d3dcbca48e714b3
Elard Huaraca <elard.sd.9@gmail.com>
2021-04-15 08:39:28 -0700

    Creacion de la Aplicacion

---
+++ CollecionDeJuegos/CollecionDeJuegos/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CollecionDeJuegos
+//
+//  Created by Elard Huaraca on 15/4/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
